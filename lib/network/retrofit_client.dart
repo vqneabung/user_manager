ed8e32e5446fa908f4e619593759dc3290a6a356
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2025-12-31 14:58:41 +0700

    Add Retrofit client implementation and update dependencies

--- lib/network/retrofit_client.dart
+++ lib/network/retrofit_client.dart
@@ -1,5 +1,6 @@
 
 import 'package:dio/dio.dart';
+import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 import 'package:user_manager/domain/dtos/response/login_response.dart';
 
@@ -8,5 +9,4 @@ part 'retrofit_client.g.dart';
 @RestApi()
 abstract class RetrofitClient {
     factory RetrofitClient(Dio dio, {String baseUrl}) = _RetrofitClient;
-
 }
