2e258b6c7e3e12b4dd03df6dc6497cb1826c4e29
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-02 21:55:38 +0700

    Add riverpod_lint plugin and update pubspec.yaml dependencies; refactor main.dart to use ProviderScope and update navigation to use RootApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:user_manager/config/injection.dart';
 import 'package:user_manager/navigation.dart';
-
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
-  runApp(const StatefulShellRouteApp());
-} 
+  runApp(const ProviderScope(child: StatefulShellRouteApp()));
+}
 
-class App extends StatelessWidget {
-  const App({super.key, required this.navigationShell});
+class RootApp extends StatelessWidget {
+  const RootApp({super.key, required this.navigationShell});
 
   final StatefulNavigationShell navigationShell;
 
